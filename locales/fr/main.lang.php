c72d1612f7e7d8c4e718588370443d1d5d30b535
franck <carnet.franck.paul@gmail.com>
2022-12-05 16:33:33 +0100

    Update locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,13 +10,21 @@
 #
 
 l10n::$locales['Insert multiple media'] = 'Insertion multiple de média';
+l10n::$locales['None'] = 'Aucun';
+l10n::$locales['div'] = '';
+l10n::$locales['p'] = '';
+l10n::$locales['aside'] = '';
+l10n::$locales['article'] = '';
+l10n::$locales['section'] = '';
+l10n::$locales['Container HTML element:'] = 'Élément HTML englobant :';
+l10n::$locales['HTML element class(es):'] = 'Classes pour l\'élément englobant :';
+l10n::$locales['Comma separated list of classes, leave it empty to not use it.'] = 'Liste de classes séparées par des virgules, laissez vide pour ne pas l\'utiliser.';
 l10n::$locales['Insertion preferences'] = 'Préférences d\'insertion';
 l10n::$locales['Image size'] = 'Taille des images';
 l10n::$locales['original'] = 'originale';
 l10n::$locales['Image legend and title'] = 'Légende et titre des images';
 l10n::$locales['Legend and title'] = 'Titre et légende';
 l10n::$locales['Title'] = 'Titre';
-l10n::$locales['None'] = 'Aucun';
 l10n::$locales['Image alignment'] = 'Alignement des images';
 l10n::$locales['Left'] = 'Gauche';
 l10n::$locales['Right'] = 'Droite';
