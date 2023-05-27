80ef7b9a079661a8c0709ed62d8a1eb8a5d8440e
franck <carnet.franck.paul@gmail.com>
2023-05-27 16:43:13 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['multipleMedia'] = '';
+L10n::$locales['Insert multiple media'] = '';
+L10n::$locales['None'] = '';
+L10n::$locales['div'] = '';
+L10n::$locales['p'] = '';
+L10n::$locales['aside'] = '';
+L10n::$locales['article'] = '';
+L10n::$locales['section'] = '';
+L10n::$locales['Container HTML element:'] = '';
+L10n::$locales['Social sharing buttons text prefix:'] = '';
+L10n::$locales['Comma separated list of classes, leave it empty to not use it.'] = '';
+L10n::$locales['original'] = '';
+L10n::$locales['Left'] = '';
+L10n::$locales['Right'] = '';
+L10n::$locales['Center'] = '';
+L10n::$locales['Insertion preferences'] = '';
+L10n::$locales['Image size'] = '';
+L10n::$locales['Image legend and title'] = '';
+L10n::$locales['Legend and title'] = '';
+L10n::$locales['Title'] = '';
+L10n::$locales['Image alignment'] = '';
+L10n::$locales['Image insertion'] = '';
+L10n::$locales['As a single image'] = '';
+L10n::$locales['As a link to the original image'] = '';
+L10n::$locales['Insert'] = '';
+L10n::$locales['Cancel'] = '';
