916403af9f0fdf3e74577e83d6cd3e4526703057
franck <carnet.franck.paul@gmail.com>
2023-06-28 12:31:33 +0200

    Fix media center alignment

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'multipleMedia',
     'Insert multiple media',
     'Franck Paul',
-    '3.3.1',
+    '3.3.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
