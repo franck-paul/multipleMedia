a3a14f0d8689af2fff2a6bd1aeaf608b9e247f69
franck <carnet.franck.paul@gmail.com>
2023-12-09 13:04:45 +0100

    New release 5.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'multipleMedia',
     'Insert multiple media',
     'Franck Paul',
-    '5.2',
+    '5.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
