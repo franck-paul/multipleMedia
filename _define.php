918cee93585d3498e7e9b16301410084bc986e50
franck <contact@open-time.net>
2026-08-13 16:43:36 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,25 +11,29 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'multipleMedia',
-    'Insert multiple media',
-    'Franck Paul',
-    '8.0.1',
-    [
-        'date'        => '2026-08-10T10:54:04+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'type'        => 'plugin',
-        'settings'    => [
-            'self' => false,                    // index.php is only used for popup action
-            'blog' => '#params.multiplemedia',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://github.com/franck-paul/multipleMedia',
-        'support'    => 'https://github.com/franck-paul/multipleMedia',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/multipleMedia/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'multipleMedia',
+        'Insert multiple media',
+        'Franck Paul',
+        '8.0.1',
+        [
+            'date'        => '2026-08-10T10:54:04+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+            'type'        => 'plugin',
+            'settings'    => [
+                'self' => false,                    // index.php is only used for popup action
+                'blog' => '#params.multiplemedia',
+            ],
+
+            'details'    => 'https://github.com/franck-paul/multipleMedia',
+            'support'    => 'https://github.com/franck-paul/multipleMedia',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/multipleMedia/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
